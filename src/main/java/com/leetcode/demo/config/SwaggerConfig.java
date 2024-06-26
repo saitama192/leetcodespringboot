0b801dd58d5c4c7c01203c105da7183b85a879e5
saitama192 <68859410+saitama192@users.noreply.github.com>
2024-06-26 09:50:18 +0200

    adding paranthesis endpoint and tests

---
+++ src/main/java/com/leetcode/demo/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.leetcode.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.OAS_30)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.leetcode.demo"))
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("My API")
+//                .description("API documentation")
+//                .version("1.0.0")
+//                .build();
+//    }
+}
